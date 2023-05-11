1f5c628667621d9dfd820701097ad443a7c5efef
husush23 <husseinkadare40@gmail.com>
2023-05-11 15:26:10 +0300

    Added new files and fixed errors

---
+++ src/modules/helpers.js
@@ -0,0 +1,14 @@
+// import {todo} from './constants.js';
+const todos = JSON.parse(localStorage.getItem('todos'));
+// const todos = localStorage.getItem('todos');
+export default function setIndex() {
+  const todo = todos;
+  if (todo) {
+    for (let i = 0; i < todos.length; i += 1) {
+      todos[i].index = i + 1;
+    }
+  } else return;
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+console.log(todos);
